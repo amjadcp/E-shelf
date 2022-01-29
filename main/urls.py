f02508cd90786cd936551ffc299b5c05baa1b7e7
Amjad C P <84246684+amjadcp@users.noreply.github.com>
2022-01-29 06:56:41 +0530

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('UserProfile.urls', namespace='UserProfile')),
+    path('dash_board/', include('Room.urls', namespace='Room')),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
