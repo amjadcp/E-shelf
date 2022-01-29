92f7c3a4e0c91613a2e91f6f456d66f8300996c6
Amjad C P <84246684+amjadcp@users.noreply.github.com>
2022-01-29 06:58:51 +0530

    Update views.py

--- Room/views.py
+++ Room/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from .models import *
 
 # Create your views here.
+
+@login_required(login_url='/login/')
+def dash_board(request):
+    email = request.user.email
+    print(Room.objects.filter(creator=email))
+    return render(request, 'dash.html')
